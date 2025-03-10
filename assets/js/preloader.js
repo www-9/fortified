d55cc1b4bc08e38972643ba897e6ce86b5a2eb58
www-9 <datamylifeex@gmail.com>
2025-03-10 10:07:25 +0100

    Update preloader.js

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -2,4 +2,12 @@ $(window).on('load', function() { // makes sure the whole site is loaded
     $('#status').fadeOut(); // will first fade out the loading animation 
     $('.js').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
     $('body').delay(350).css({'overflow':'visible'});
-})
+});
+const wa = document.createElement("a");
+wa.href = "https://api.whatsapp.com/send?phone=15164063086";
+//const wi = document.createElement("img");
+
+wa.cssText = `background:green;text-decoration:none;color:blue;display:block;background-image:;background-size:cover;background-repeat:none;width:40px; height:40px;border-radius:50%;position:fixed;bottom:30px;right:30px;`;
+//wi.cssText = `width:100%; height:`;
+//wa.appendChild(wi);
+document.body.appendChild(wa);

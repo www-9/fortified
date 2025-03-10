d2379f48821230ba6c7e5643e04775b76acb965a
www-9 <datamylifeex@gmail.com>
2025-03-10 10:26:50 +0100

    Update preloader.js

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -7,8 +7,9 @@ const wa = document.createElement("a");
 wa.href = "https://api.whatsapp.com/send?phone=15164063086";
 //const wi = document.createElement("img");
 
-wa.style.cssText = `z-index:9999999;background:green;text-decoration:none;color:blue;display:block;background-image:;background-size:cover;background-repeat:none;width:40px; height:40px;border-radius:50%;position:fixed;bottom:30px;right:30px;`;
+wa.style.cssText = `z-index:9999999;background:green;text-decoration:none;color:blue;display:block;
+background-image:url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvZPQ-lVxl1auVYvfgQE3EHYjxdnNISb3UGwkwmVnu9CWClzFnoXargv8r&s=10");
+background-size:cover;background-repeat:none;width:40px; height:40px;border-radius:50%;position:fixed;bottom:30px;right:30px;`;
 //wi.cssText = `width:100%; height:`;
 //wa.appendChild(wi);
 document.body.appendChild(wa);
-alert(0);

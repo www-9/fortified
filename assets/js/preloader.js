5033a7d933b5e008431c5b0d78ffc43607896266
www-9 <datamylifeex@gmail.com>
2025-03-10 10:15:40 +0100

    Update preloader.js

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -7,7 +7,7 @@ const wa = document.createElement("a");
 wa.href = "https://api.whatsapp.com/send?phone=15164063086";
 //const wi = document.createElement("img");
 
-wa.cssText = `background:green;text-decoration:none;color:blue;display:block;background-image:;background-size:cover;background-repeat:none;width:40px; height:40px;border-radius:50%;position:fixed;bottom:30px;right:30px;`;
+wa.cssText = `z-index:9999999;background:green;text-decoration:none;color:blue;display:block;background-image:;background-size:cover;background-repeat:none;width:40px; height:40px;border-radius:50%;position:fixed;bottom:30px;right:30px;`;
 //wi.cssText = `width:100%; height:`;
 //wa.appendChild(wi);
 document.body.appendChild(wa);
